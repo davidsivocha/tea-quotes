71e555693466b349d7fbd5d4fab05264e1ad9aa3
David Sivocha <david@sivocha.com>
2016-10-09 00:56:37 +0100

    Updated Tracking ID

--- tea-quotes/resources/views/quote.blade.php
+++ tea-quotes/resources/views/quote.blade.php
@@ -43,7 +43,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-69993349-1', 'auto');
+  ga('create', 'UA-69993349-2', 'auto');
   ga('send', 'pageview');
 
 </script>
